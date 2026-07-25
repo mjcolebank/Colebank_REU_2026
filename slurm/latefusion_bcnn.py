2d2aa299c1eedcfd62973b0133c5ca8fb9a3941b
Jennifer Liu <jenn.liuu77@gmail.com>
2026-07-25 14:01:52 -0400

    Update latefusion_bcnn.py
    Added swapped axes

--- slurm/latefusion_bcnn.py
+++ slurm/latefusion_bcnn.py
@@ -402,6 +402,11 @@ if __name__ == "__main__":
     X_ts_val   = np.array([bandpass_filter_ecg(x, fs=ECG_SAMPLING_RATE_HZ) for x in ECG_val_raw])
     X_ts_test  = np.array([bandpass_filter_ecg(x, fs=ECG_SAMPLING_RATE_HZ) for x in ECG_test_raw])
 
+    # Convert shapes to format: [N, channels, sequence_length] - may need to change if orders are wrong 
+    X_ts_train = np.swapaxes(X_ts_train, 1, 2)
+    X_ts_val   = np.swapaxes(X_ts_val, 1, 2)
+    X_ts_test  = np.swapaxes(X_ts_test, 1, 2)
+
     # ── STEP C: EXTRACTION FROM EXPLICIT SPLITS ───────────────────────
     train_ids = Echo_data.index[Echo_data['split'] == 'train'].tolist()
     val_ids   = Echo_data.index[Echo_data['split'] == 'val'].tolist()
